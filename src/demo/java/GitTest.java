d4493ce8345a381f2a54a16a7b755be0a0ebe430
LH <2908267418@qq.com>
2022-07-10 21:01:49 +0800

    hot-first2 commit2

--- src/demo/java/GitTest.java
+++ src/demo/java/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("git  配制");
         System.out.println("git 第三版本");
         System.out.println("git-hot 第1版本");
+        System.out.println("git-hot 第2版本");
+        System.out.println("git-hot 第2版本");
     }
 }
