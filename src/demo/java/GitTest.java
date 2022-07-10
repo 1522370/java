984ed00643e263841b09e49b471f20c10aeb3c4e
LH <2908267418@qq.com>
2022-07-10 17:31:50 +0800

    third commit

--- src/demo/java/GitTest.java
+++ src/demo/java/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git成功配制");
         System.out.println("git  配制");
+        System.out.println("git 第三版本");
     }
 }
